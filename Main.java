f8cabbbd6ad0461afbfb2d70d4380304f9c04fba
izanlopezbc <izan.lopez@insbaixcamp.cat>
2025-05-17 16:11:21 +0200

    Canvis main i biblioteca finals

--- Main.java
+++ Main.java
@@ -1,17 +1,73 @@
+import java.util.Scanner;
 
 public class Main {
-
+     
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Biblioteca biblioteca = new Biblioteca(); 
-        Llibre llibre1 = new Llibre("1984", "George Orwell");
-        Llibre llibre2 = new Llibre("El petit príncep", "Antoine de Saint-Exupéry");
 
-        biblioteca.afegirLlibre(llibre1);
-        biblioteca.afegirLlibre(llibre2);
+        boolean sortir = false;
+        int opcio;
 
-        Usuari usuari = new Usuari("Carla");
+        System.out.println("Com et dius:");
+        Usuari usuari = new Usuari(scanner.next());
         GestorBiblioteca gestor = new GestorBiblioteca();
-        
-        gestor.prestarLlibre(usuari, llibre1);
+
+        while (!sortir) {
+            System.out.println("Gestió de llibres");
+            System.out.println("1. Afegir llibre");
+            System.out.println("2. Prestar llibre");
+            System.out.println("3. Eliminar llibre");
+            System.out.println("0. Sortir");
+            System.out.print("Opció: ");
+            opcio = scanner.nextInt();
+            scanner.nextLine();
+
+             switch (opcio) {
+                case 1:
+                    System.out.print("Títol: ");
+                    String titol = scanner.nextLine();
+                    System.out.print("Autor: ");
+                    String autor = scanner.nextLine();
+                    
+                    Llibre llibre = new Llibre(titol, autor);
+
+                    biblioteca.afegirLlibre(llibre);
+                    
+                    break;
+                    
+                case 2:
+                System.out.print("Quin llibre vols prestar ");
+
+                String llibreABuscar = scanner.nextLine();
+
+                    if (llibreABuscar != null) {
+                       gestor.prestarLlibre(usuari, biblioteca.buscarLlibre(llibreABuscar));
+                    } else {
+                        System.out.println("Llibre no trobat.");
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Quin llibre vols eliminar: ");
+                    String titolAEliminar = scanner.nextLine();
+                    Llibre llibreAEliminar = biblioteca.buscarLlibre(titolAEliminar);
+                   
+                    if (llibreAEliminar != null) {
+                        biblioteca.getLlibres().remove(llibreAEliminar);
+                        System.out.println("Llibre eliminat.");
+                    } else {
+                        System.out.println("Llibre no trobat.");
+                    }
+                    break;
+               
+                case 0:
+                    sortir = true;
+                    break;
+
+                default:
+                    System.out.println("Aquesta opció no existeix");
+            }
+        }
     }
 }
